855b19618b1772070e746a0c8f878ab82df3ba1e
franjofranjic27 <franjic.f@bluewin.ch>
2025-10-12 21:34:24 +0200

    working on helm chart

---
+++ market-data-service/src/integration-test/java/mds/BaseIntegrationTest.java
@@ -0,0 +1,9 @@
+package ch.ost.clde.mds;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("integration-test")
+public abstract class BaseIntegrationTest {
+}
